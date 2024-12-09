cedbb367204f5e2af9211724390cc808124c0fd6
Samuu10 <svallmun@myuax.com>
2024-12-09 18:44:26 +0100

    Configuracion de conexion

--- src/main/java/com/example/wakandacentral/ServidorEurekaApplication.java
+++ src/main/java/com/example/wakandacentral/ServidorEurekaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class WakandaCentralApplication {
+public class ServidorEurekaApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(WakandaCentralApplication.class, args);
+		SpringApplication.run(ServidorEurekaApplication.class, args);
 	}
 }
--- src/test/java/com/example/wakandacentral/ServidorEurekaApplicationTests.java
+++ src/test/java/com/example/wakandacentral/ServidorEurekaApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WakandaCentralApplicationTests {
+class ServidorEurekaApplicationTests {
 
 	@Test
 	void contextLoads() {
